d6b27cc18e19d9d03097024f1518964375b9eb52
geolunalg <geovanni_luna@yahoo.com>
2026-08-13 03:31:33 +0000

    parse and process headers in request call

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -16,6 +16,8 @@ func NewHeaders() Headers {
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	// print the data with crlf encoding
+
 	idx := bytes.Index(data, []byte(crlf))
 	if idx == -1 {
 		return 0, false, nil
@@ -60,19 +62,12 @@ var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', 
 // or characters that are allowed in a token
 func validTokens(data []byte) bool {
 	for _, c := range data {
-		if !isTokenChar(c) {
+		if !(c >= 'A' && c <= 'Z' ||
+			c >= 'a' && c <= 'z' ||
+			c >= '0' && c <= '9' ||
+			slices.Contains(tokenChars, c)) {
 			return false
 		}
 	}
 	return true
-}
-
-func isTokenChar(c byte) bool {
-	if c >= 'A' && c <= 'Z' ||
-		c >= 'a' && c <= 'z' ||
-		c >= '0' && c <= '9' {
-		return true
-	}
-
-	return slices.Contains(tokenChars, c)
 }
